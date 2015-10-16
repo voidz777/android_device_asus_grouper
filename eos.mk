ed5e05a0d226d51d7aa52824e2f299d72a1b85da
Altaf-Mahdi <altaf.mahdi@gmail.com>
2015-10-16 15:41:35 +0100

    grouper: eos bring up
    Change-Id: I1343119bb2f5216f2d89a3e0747fcc4e1a7bf7bd

---
+++ eos.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus7
+
+# Inherit AOSP device configuration
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+# Inherit common product files.
+$(call inherit-product, vendor/eos/config/common.mk)
+
+# Inherit tablet config.
+$(call inherit-product, vendor/eos/config/common_full_tablet_wifionly.mk)
+
+# Setup device specific product configuration.
+PRODUCT_DEVICE := grouper
+PRODUCT_NAME := eos_grouper
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.1/LMY47D/1743759:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.1 LMY47D 1743759 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1280x800.zip:system/media/bootanimation.zip
